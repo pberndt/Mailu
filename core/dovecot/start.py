5ea38c54113e0c1080e21a932d0898e5e7be52fa
pberndt <pberndt@users.noreply.github.com>
2020-06-04 18:39:38 +0200

    Remove unneeded dependency on WEBMAIL

--- core/dovecot/start.py
+++ core/dovecot/start.py
@@ -26,8 +26,6 @@ os.environ["FRONT_ADDRESS"] = system.get_host_address_from_environment("FRONT", 
 os.environ["REDIS_ADDRESS"] = system.get_host_address_from_environment("REDIS", "redis")
 os.environ["ADMIN_ADDRESS"] = system.get_host_address_from_environment("ADMIN", "admin")
 os.environ["ANTISPAM_WEBUI_ADDRESS"] = system.get_host_address_from_environment("ANTISPAM_WEBUI", "antispam:11334")
-if os.environ["WEBMAIL"] != "none":
-    os.environ["WEBMAIL_ADDRESS"] = system.get_host_address_from_environment("WEBMAIL", "webmail")
 
 for dovecot_file in glob.glob("/conf/*.conf"):
     conf.jinja(dovecot_file, os.environ, os.path.join("/etc/dovecot", os.path.basename(dovecot_file)))
